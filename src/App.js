27ed8d8f02ec47af919c139b0dd05fcf04be7910
Shubhi1507 <shubhisingh1506@gmail.com>
2024-05-10 00:23:59 +0530

    app js updated

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
-    // window.open("https://yesmadamdeeplink.page.link/eNh4");
-    if (confirm("go to app"))
+    if (confirm("go to app")) {
       document.location = "https://yesmadamdeeplink.page.link/eNh4";
-    else document.location = "yesmadamapp://";
+    } else {
+      document.location = "yesmadamapp://";
+    }
   }, []);
   return <div className="App"></div>;
 }
