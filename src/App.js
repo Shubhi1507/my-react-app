683c1189e0587efb4ba46c51bdcd7a5c928c0034
Shubhi1507 <shubhisingh1506@gmail.com>
2024-05-10 00:10:17 +0530

    url

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
-    window.open("https://yesmadamdeeplink.page.link/eNh4");
-    // if (confirm("go to app"))
-    //   document.location = "https://yesmadamdeeplink.page.link/eNh4";
+    // window.open("https://yesmadamdeeplink.page.link/eNh4");
+    if (confirm("go to app"))
+      document.location = "https://yesmadamdeeplink.page.link/eNh4";
+    else document.location = "yesmadamapp://";
   }, []);
   return <div className="App"></div>;
 }
